e914179a809f39b0d4982cf273d3c99519ed590d
Anshul Arora <a.99.anshul@gmail.com>
2020-09-22 19:08:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,11 +6,11 @@ def key_for_min_value(name_hash)
     return nil
   end
   
-  lowest_price = name_hash.values[0]
+  lowest_price = name_hash
   
   name_hash.each do |item, price|
     if price < lowest_price
-      return price
+      return item
     end
   end
 end
