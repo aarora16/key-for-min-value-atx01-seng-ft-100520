c26e15bacba087003a1f436840e055a3f58473bc
Anshul Arora <a.99.anshul@gmail.com>
2020-09-22 19:13:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,12 @@ def key_for_min_value(name_hash)
   end
   
   lowest_pair = name_hash.first
+  lowest_key = lowest_pair[0]
   lowest_price = lowest_pair[1]
   
   name_hash.each do |item, price|
-    if price <= lowest_price
-      return item
+    if price < lowest_price
+      lowest_pair = [item, price]
     end
   end
 end
